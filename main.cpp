97788d11b702f4d96e62505316bca8624452c763
Blue <99059036+bluchv@users.noreply.github.com>
2024-12-27 13:03:10 -0500

    Upgraded to /std-c++20 - Removed "validGames" array

--- main.cpp
+++ main.cpp
@@ -1,29 +1,27 @@
 #include <iostream>
 #include <array>
 #include <unordered_map>
+#include <map>
 #include <functional>
 #include "games/number_guessing.cpp"
- 
-using namespace std;
 
-string validGames[] = {
-	"number-guessing"
-};
+using namespace std;
 
 int main()
 {
 	std::unordered_map<std::string, std::function<int()>> funcMap;
-	funcMap["number-guessing"] = start_guess_game;
+	funcMap["number-guess"] = start_guess_game;
 
 	int gamesPlayed = 0;
 	int cash = 0;
-	int size = sizeof(validGames) / sizeof(validGames[0]); 
+	int size = funcMap.size(); 
+	int i = 0;
 
 	string game;
 	string allowedGames = "";
-	
-	for (int i = 0; i < size; i++) {
-		string validGame = validGames[i];
+
+	for (auto [validGame, fn] : funcMap) {
+		i += 1;
 		if (allowedGames != "")
 		{
 			if (i + 1 >= size)
@@ -53,7 +51,7 @@ int main()
 		std::cout << "Choose your game type. Options: " << allowedGames;
 		std::cin >> game;
 
-		if (std::find(std::begin(validGames), std::end(validGames), game) == std::end(validGames))
+		if (!funcMap.contains(game))
 		{
 			std::cout << "You idiot thats not a valid option" << std::endl;
 		}
